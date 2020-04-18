57d10f94d03a877748d837224e27c97c72aa9ea9
Pedro Jr <prdsj91@gmail.com>
2020-04-18 02:37:35 -0300

    clone picpay

---
+++ src/screens/Pay/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import { Text, View } from "react-native";
+
+export default function Pay() {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Pay</Text>
+    </View>
+  );
+}
---
+++ src/screens/Wallet/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import { Text, View } from "react-native";
+
+export default function Wallet() {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Wallet</Text>
+    </View>
+  );
+}
